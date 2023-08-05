34dbe0fdb43a7a0eeab85f9412273a43ec295f6d
eemanDigital <eemandigitalconcept@gamil.com>
2023-08-05 14:58:31 +0100

    tasks completed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,12 +4,14 @@
 This module contains a function to determine if all the boxes can be opened.
 """
 
+
 def canUnlockAll(boxes):
     """
     Determine if all the boxes can be opened.
 
     Args:
-        boxes (list): A list of lists. Each inner list represents a box and contains keys.
+        boxes (list): A list of lists. Each inner list
+        represents a box and contains keys.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
